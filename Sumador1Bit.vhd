0ea010c493780ab8dbc26399dffd18224be8a08a
alexenr02 <57109942+alexenr02@users.noreply.github.com>
2021-02-22 16:21:41 -0600

    Create Sumador1Bit.vhd

---
+++ Sumador1Bit.vhd
@@ -0,0 +1,28 @@
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity sumador1 is
+    Port ( a : in STD_LOGIC;
+           b : in STD_LOGIC;
+           cin : in STD_LOGIC;
+           s : out STD_LOGIC;
+           cout : out STD_LOGIC);
+end sumador1;
+
+architecture Behavioral of sumador1 is
+
+begin
+s <= a xor b xor cin;
+    cout <= (a and b) or (cin and (a or b) );
+    
+end Behavioral;
